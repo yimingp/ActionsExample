e8ad6003c2cb32e69abebf30e2a26deb2ffc69b2
Mark <markelliottfontenot@gmail.com>
2020-01-25 08:17:49 -0600

    cleanded up the build.yaml file and added file output to main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,22 @@
 #include <iostream>
+#include <fstream>
+
+using std::ofstream;
+using std::cout;
+using std::endl;
 
 int main() {
   for (int i = 0; i < 10; i++) {
-    std::cout << "Hello CS Elite Coders! " << i << std::endl;
+    cout << "Hello CS Elite Coders! " << i << endl;
   }
+
+  ofstream fout ("myfile.txt");
+  if (!fout) {
+      return -1;
+  }
+
+  fout << "Hello world from a file!" << endl;
+  fout.close();
 
   return 0;
 }
