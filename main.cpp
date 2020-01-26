f1dbf3508309144cf21756a73805a973919beeac
Mark <markelliottfontenot@gmail.com>
2020-01-26 05:53:48 -0600

    added cmake command to copy input file to build directory.

--- main.cpp
+++ main.cpp
@@ -5,18 +5,23 @@ using std::ofstream;
 using std::cout;
 using std::endl;
 
-int main() {
-  for (int i = 0; i < 10; i++) {
-    cout << "Hello CS Elite Coders! " << i << endl;
-  }
+int main(int argc, char **argv) {
 
-  ofstream fout ("myfile.txt");
-  if (!fout) {
-      return -1;
-  }
+    cout << "Printing the command line args..." << endl;
+    for (int i = 0; i < argc; i++)
+        cout << "arg[" << i << "] = " << argv[i] << endl;
 
-  fout << "Hello world from a file!" << endl;
-  fout.close();
+    for (int i = 0; i < 10; i++) {
+        cout << "Hello CS Elite Coders! " << i << endl;
+    }
 
-  return 0;
+    ofstream fout("myfile.txt");
+    if (!fout) {
+        return -1;
+    }
+
+    fout << "Hello world from a file!" << endl;
+    fout.close();
+
+    return 0;
 }
