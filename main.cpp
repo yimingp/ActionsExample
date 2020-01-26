5eb9101229a4ad1d7fb0997ce878edac735a7e07
Mark <mfonten@lyle.smu.edu>
2020-01-26 07:26:53 -0600

    trying an Environment variable to have cmd line args

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 
 using std::ofstream;
+using std::ifstream;
 using std::cout;
 using std::endl;
 
@@ -14,6 +15,17 @@ int main(int argc, char **argv) {
     for (int i = 0; i < 10; i++) {
         cout << "Hello CS Elite Coders! " << i << endl;
     }
+
+    ifstream fin(argv[1]);
+    if (!fin) {
+        cout << "Cannot open file" << endl;
+    }
+
+    char buffer[80];
+    while (fin >> buffer) {
+        cout << buffer << endl;
+    }
+    fin.close();
 
     ofstream fout("myfile.txt");
     if (!fout) {
